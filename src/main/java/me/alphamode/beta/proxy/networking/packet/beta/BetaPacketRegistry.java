4544d55fc573f32f249b07157945a54467996bcd
lowercasebtw <jejojora2@gmail.com>
2026-01-20 18:13:21 -0500

    AddPaintingPacket

--- src/main/java/me/alphamode/beta/proxy/networking/packet/beta/BetaPacketRegistry.java
+++ src/main/java/me/alphamode/beta/proxy/networking/packet/beta/BetaPacketRegistry.java
@@ -62,7 +62,7 @@ public class BetaPacketRegistry {
 //		this.registerPacket(BetaPackets.TAKE_ITEM_ENTITY, TakeItemEntityPacket.CODEC);
 //		this.registerPacket(BetaPackets.ADD_ENTITY, AddEntityPacket.CODEC);
 		this.registerPacket(BetaPackets.ADD_MOB, AddMobPacket.CODEC);
-//		this.registerPacket(BetaPackets.ADD_PAINTING, AddPaintingPacket.CODEC);
+		this.registerPacket(BetaPackets.ADD_PAINTING, AddPaintingPacket.CODEC);
 //		this.registerPacket(BetaPackets.PLAYER_INPUT, PlayerInputPacket.CODEC);
 		this.registerPacket(BetaPackets.SET_ENTITY_MOTION, SetEntityMotionPacket.CODEC);
 //		this.registerPacket(BetaPackets.REMOVE_ENTITY, RemoveEntityPacket.CODEC);
--- src/main/java/me/alphamode/beta/proxy/networking/packet/beta/packets/AddPaintingPacket.java
+++ src/main/java/me/alphamode/beta/proxy/networking/packet/beta/packets/AddPaintingPacket.java
@@ -1,45 +1,28 @@
 package me.alphamode.beta.proxy.networking.packet.beta.packets;
 
 import io.netty.buffer.ByteBuf;
+import me.alphamode.beta.proxy.networking.packet.beta.BetaPackets;
 import me.alphamode.beta.proxy.util.codec.ByteBufCodecs;
 import me.alphamode.beta.proxy.util.codec.StreamCodec;
 import me.alphamode.beta.proxy.util.data.Vec3i;
-import net.raphimc.netminecraft.packet.Packet;
 
-public class AddPaintingPacket implements Packet {
+public record AddPaintingPacket(int id, String motive, Vec3i position, int direction) implements RecordPacket {
 	public static final int MAX_PAINTING_MOTIVE = 13; // 13 is Painting.Motive.length
 	public static final StreamCodec<ByteBuf, String> MOTIVE_CODEC = ByteBufCodecs.stringUtf8(MAX_PAINTING_MOTIVE);
-
-	public int id;
-	public Vec3i position;
-	public int dir;
-	public String motive;
-
-	public AddPaintingPacket() {
-	}
-
-//    public AddPaintingPacket(Painting painting) {
-//        this.id = painting.id;
-//        this.x = painting.xTile;
-//        this.y = painting.yTile;
-//        this.z = painting.zTile;
-//        this.dir = painting.direction;
-//        this.motive = painting.motive.name;
-//    }
+	public static final StreamCodec<ByteBuf, AddPaintingPacket> CODEC = StreamCodec.composite(
+			ByteBufCodecs.INT,
+			AddPaintingPacket::id,
+			MOTIVE_CODEC,
+			AddPaintingPacket::motive,
+			Vec3i.CODEC,
+			AddPaintingPacket::position,
+			ByteBufCodecs.INT,
+			AddPaintingPacket::direction,
+			AddPaintingPacket::new
+	);
 
 	@Override
-	public void read(final ByteBuf buf, final int protocolVersion) {
-		this.id = buf.readInt();
-		this.motive = MOTIVE_CODEC.decode(buf);
-		this.position = Vec3i.CODEC.decode(buf);
-		this.dir = buf.readInt();
-	}
-
-	@Override
-	public void write(final ByteBuf buf, final int protocolVersion) {
-		buf.writeInt(this.id);
-		MOTIVE_CODEC.encode(buf, this.motive);
-		Vec3i.CODEC.encode(buf, this.position);
-		buf.writeInt(this.dir);
+	public BetaPackets getType() {
+		return BetaPackets.ADD_PAINTING;
 	}
 }
