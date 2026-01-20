03b6ba05350f727655af16cc9bc90a7db1864215
lowercasebtw <jejojora2@gmail.com>
2026-01-20 18:54:38 -0500

    InteractionPacket, SetCarriedItemPacket, UseItemPacket

--- src/main/java/me/alphamode/beta/proxy/networking/packet/beta/packets/SetCarriedItemPacket.java
+++ src/main/java/me/alphamode/beta/proxy/networking/packet/beta/packets/SetCarriedItemPacket.java
@@ -1,25 +1,19 @@
 package me.alphamode.beta.proxy.networking.packet.beta.packets;
 
 import io.netty.buffer.ByteBuf;
-import net.raphimc.netminecraft.packet.Packet;
+import me.alphamode.beta.proxy.networking.packet.beta.BetaPackets;
+import me.alphamode.beta.proxy.util.codec.ByteBufCodecs;
+import me.alphamode.beta.proxy.util.codec.StreamCodec;
 
-public class SetCarriedItemPacket implements Packet {
-	public int slot;
-
-	public SetCarriedItemPacket() {
-	}
-
-	public SetCarriedItemPacket(final int slot) {
-		this.slot = slot;
-	}
-
-	@Override
-	public void read(final ByteBuf buf, final int protocolVersion) {
-		this.slot = buf.readShort();
-	}
+public record SetCarriedItemPacket(short slot) implements RecordPacket {
+	public static final StreamCodec<ByteBuf, SetCarriedItemPacket> CODEC = StreamCodec.composite(
+			ByteBufCodecs.SHORT,
+			SetCarriedItemPacket::slot,
+			SetCarriedItemPacket::new
+	);
 
 	@Override
-	public void write(final ByteBuf buf, final int protocolVersion) {
-		buf.writeShort(this.slot);
+	public BetaPackets getType() {
+		return BetaPackets.SET_CARRIED_ITEM;
 	}
 }
