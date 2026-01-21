51927b3615b4844ed3cd30dcc14c8db9051869c0
lowercasebtw <jejojora2@gmail.com>
2026-01-21 12:51:03 -0500

    fix packet state enum

--- src/main/java/me/alphamode/beta/proxy/networking/packet/modern/PacketState.java
+++ src/main/java/me/alphamode/beta/proxy/networking/packet/modern/PacketState.java
@@ -2,8 +2,8 @@ package me.alphamode.beta.proxy.networking.packet.modern;
 
 public enum PacketState {
 	HANDSHAKING,
-	LOGIN,
-	CONFIGURATION,
 	PLAY,
-	ENCRYPTION
+	STATUS,
+	LOGIN,
+	CONFIGURATION
 }
