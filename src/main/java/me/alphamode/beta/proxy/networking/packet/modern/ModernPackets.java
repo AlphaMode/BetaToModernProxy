a8faf9b8e1dde14bcf146f5276f05caff7218a95
lowercasebtw <jejojora2@gmail.com>
2026-01-21 13:19:35 -0500

    e

--- src/main/java/me/alphamode/beta/proxy/networking/packet/modern/ModernPackets.java
+++ src/main/java/me/alphamode/beta/proxy/networking/packet/modern/ModernPackets.java
@@ -4,32 +4,38 @@ import me.alphamode.beta.proxy.networking.packet.Packets;
 import me.alphamode.beta.proxy.util.Identifier;
 import me.alphamode.beta.proxy.util.Tuple;
 
+import java.util.List;
 import java.util.Map;
 
 public enum ModernPackets implements Packets {
 	HANDSHAKING(Map.of(
-
+			PacketDirection.SERVERBOUND, List.of(),
+			PacketDirection.CLIENTBOUND, List.of()
 	)),
 
 	PLAY(Map.of(
-
+			PacketDirection.SERVERBOUND, List.of(),
+			PacketDirection.CLIENTBOUND, List.of()
 	)),
 
 	STATUS(Map.of(
-
+			PacketDirection.SERVERBOUND, List.of(),
+			PacketDirection.CLIENTBOUND, List.of()
 	)),
 
 	LOGIN(Map.of(
-
+			PacketDirection.SERVERBOUND, List.of(),
+			PacketDirection.CLIENTBOUND, List.of()
 	)),
 
 	CONFIGURATION(Map.of(
-
+			PacketDirection.SERVERBOUND, List.of(),
+			PacketDirection.CLIENTBOUND, List.of()
 	));
 
-	private final Map<Integer, Map<PacketDirection, Tuple<Integer, Identifier>>> packetMap;
+	private final Map<PacketDirection, List<Tuple<Integer, Identifier>>> packets;
 
-	ModernPackets(final Map<Integer, Map<PacketDirection, Tuple<Integer, Identifier>>> packetMap) {
-		this.packetMap = packetMap;
+	ModernPackets(final Map<PacketDirection, List<Tuple<Integer, Identifier>>> packetMap) {
+		this.packets = packetMap;
 	}
 }
