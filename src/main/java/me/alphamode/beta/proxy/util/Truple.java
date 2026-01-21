198897485e43d0416c03339e99d73864a10edee5
lowercasebtw <jejojora2@gmail.com>
2026-01-21 14:04:36 -0500

    na

--- src/main/java/me/alphamode/beta/proxy/util/Truple.java
+++
@@ -1,4 +0,0 @@
-package me.alphamode.beta.proxy.util;
-
-public record Truple<T, S, V>(T first, S second, V third) {
-}
--- src/main/java/me/alphamode/beta/proxy/util/Tuple.java
+++
@@ -1,4 +0,0 @@
-package me.alphamode.beta.proxy.util;
-
-public record Tuple<T, S>(T first, S second) {
-}
