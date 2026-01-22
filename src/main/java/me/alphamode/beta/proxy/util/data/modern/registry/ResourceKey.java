da9225f2d3af21e257d4ef312e4d4d4aeca656a7
lowercasebtw <jejojora2@gmail.com>
2026-01-22 15:06:12 -0500

    meow

--- src/main/java/me/alphamode/beta/proxy/util/data/modern/registry/ResourceKey.java
+++ src/main/java/me/alphamode/beta/proxy/util/data/modern/registry/ResourceKey.java
@@ -4,37 +4,33 @@ import io.netty.buffer.ByteBuf;
 import me.alphamode.beta.proxy.util.codec.StreamCodec;
 import net.lenni0451.mcstructs.core.Identifier;
 
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 public class ResourceKey<T> {
-	private static final ConcurrentMap<InternKey, ResourceKey<?>> VALUES = new MapMaker().weakValues().makeMap();
+	private static final ConcurrentMap<InternKey, ResourceKey<?>> VALUES = new ConcurrentHashMap<>();
 	private final Identifier registryName;
 	private final Identifier identifier;
 
-	private ResourceKey(Identifier registryName, Identifier identifier) {
+	private ResourceKey(final Identifier registryName, final Identifier identifier) {
 		this.registryName = registryName;
 		this.identifier = identifier;
 	}
 
-	public static <T> StreamCodec<ByteBuf, ResourceKey<T>> codec(ResourceKey<? extends Registry<T>> registryName) {
+	public static <T> StreamCodec<ByteBuf, ResourceKey<T>> codec(final ResourceKey<? extends Registry<T>> registryName) {
 		return null; // TODO: Identifier.STREAM_CODEC.map(name -> create(registryName, name), ResourceKey::identifier);
 	}
 
-	public static <T> ResourceKey<T> create(ResourceKey<? extends Registry<T>> registryName, Identifier location) {
+	public static <T> ResourceKey<T> create(final ResourceKey<? extends Registry<T>> registryName, final Identifier location) {
 		return create(registryName.identifier, location);
 	}
 
-	public static <T> ResourceKey<Registry<T>> createRegistryKey(Identifier identifier) {
+	public static <T> ResourceKey<Registry<T>> createRegistryKey(final Identifier identifier) {
 		return create(Registries.ROOT_REGISTRY_NAME, identifier);
 	}
 
-	private static <T> ResourceKey<T> create(Identifier registryName, Identifier identifier) {
+	private static <T> ResourceKey<T> create(final Identifier registryName, final Identifier identifier) {
 		return (ResourceKey<T>) VALUES.computeIfAbsent(new ResourceKey.InternKey(registryName, identifier), k -> new ResourceKey(k.registry, k.identifier));
-	}
-
-	@Override
-	public String toString() {
-		return "ResourceKey[" + this.registryName + " / " + this.identifier + "]";
 	}
 
 	public Identifier identifier() {
@@ -46,7 +42,12 @@ public class ResourceKey<T> {
 	}
 
 	public ResourceKey<Registry<T>> registryKey() {
-		return createRegistryKey(this.registryName);
+		return this.createRegistryKey(this.registryName);
+	}
+
+	@Override
+	public String toString() {
+		return "ResourceKey[" + this.registryName + " / " + this.identifier + "]";
 	}
 
 	private record InternKey(Identifier registry, Identifier identifier) {
