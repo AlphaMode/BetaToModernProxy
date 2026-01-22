3e6ae6f9905e5005840ef87c1e1f12684c5e7d09
lowercasebtw <jejojora2@gmail.com>
2026-01-22 15:59:00 -0500

    DFDF

--- src/main/java/me/alphamode/beta/proxy/networking/packet/modern/packets/s2c/common/S2CCommonUpdateTagsPacket.java
+++ src/main/java/me/alphamode/beta/proxy/networking/packet/modern/packets/s2c/common/S2CCommonUpdateTagsPacket.java
@@ -2,5 +2,5 @@ package me.alphamode.beta.proxy.networking.packet.modern.packets.s2c.common;
 
 import me.alphamode.beta.proxy.networking.packet.modern.enums.ModernClientboundPackets;
 
-public abstract class S2CUpdateTagsPacket<T extends ModernClientboundPackets> implements S2CCommonPacket<T> {
+public interface S2CCommonUpdateTagsPacket<T extends ModernClientboundPackets> extends S2CCommonPacket<T> {
 }
