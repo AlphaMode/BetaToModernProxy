d82f762c24748420318ebcbe3989eed06c0cb27c
lowercasebtw <jejojora2@gmail.com>
2026-01-21 21:23:20 -0500

    ddf

--- src/main/java/me/alphamode/beta/proxy/util/data/ByIdMap.java
+++ src/main/java/me/alphamode/beta/proxy/util/data/ByIdMap.java
@@ -7,55 +7,50 @@ import java.util.function.IntFunction;
 import java.util.function.ToIntFunction;
 
 public interface ByIdMap {
-    static <T> T[] createSortedArray(final ToIntFunction<T> idGetter, final T[] values) {
-        int length = values.length;
-        if (length == 0) {
-            throw new IllegalArgumentException("Empty value list");
-        } else {
-            T[] result = values.clone();
-            Arrays.fill(result, null);
+	static <T> T[] createSortedArray(final ToIntFunction<T> idGetter, final T[] values) {
+		final int length = values.length;
+		if (length == 0) {
+			throw new IllegalArgumentException("Empty value list");
+		} else {
+			final T[] result = values.clone();
+			Arrays.fill(result, null);
+			for (T value : values) {
+				final int id = idGetter.applyAsInt(value);
+				if (id < 0 || id >= length) {
+					throw new IllegalArgumentException("Values are not continous, found index " + id + " for value " + value);
+				}
 
-            for (T value : values) {
-                int id = idGetter.applyAsInt(value);
-                if (id < 0 || id >= length) {
-                    throw new IllegalArgumentException("Values are not continous, found index " + id + " for value " + value);
-                }
+				final T previous = result[id];
+				if (previous != null) {
+					throw new IllegalArgumentException("Duplicate entry on id " + id + ": current=" + value + ", previous=" + previous);
+				}
 
-                T previous = result[id];
-                if (previous != null) {
-                    throw new IllegalArgumentException("Duplicate entry on id " + id + ": current=" + value + ", previous=" + previous);
-                }
+				result[id] = value;
+			}
 
-                result[id] = value;
-            }
+			for (int i = 0; i < length; i++) {
+				if (result[i] == null) {
+					throw new IllegalArgumentException("Missing value at index: " + i);
+				}
+			}
 
-            for (int i = 0; i < length; i++) {
-                if (result[i] == null) {
-                    throw new IllegalArgumentException("Missing value at index: " + i);
-                }
-            }
+			return result;
+		}
+	}
 
-            return result;
-        }
-    }
+	static <T> IntFunction<T> continuous(final ToIntFunction<T> idGetter, final T[] values, final ByIdMap.OutOfBoundsStrategy strategy) {
+		final T[] sortedValues = createSortedArray(idGetter, values);
+		final int length = sortedValues.length;
+		return switch (strategy) {
+			case ZERO -> id -> id >= 0 && id < length ? sortedValues[id] : sortedValues[0];
+			case WRAP -> id -> sortedValues[Mth.positiveModulo(id, length)];
+			case CLAMP -> id -> sortedValues[Mth.clamp(id, 0, length - 1)];
+		};
+	}
 
-    static <T> IntFunction<T> continuous(final ToIntFunction<T> idGetter, final T[] values, final ByIdMap.OutOfBoundsStrategy strategy) {
-        T[] sortedValues = createSortedArray(idGetter, values);
-        int length = sortedValues.length;
-
-        return switch (strategy) {
-            case ZERO -> {
-                T zeroValue = sortedValues[0];
-                yield id -> id >= 0 && id < length ? sortedValues[id] : zeroValue;
-            }
-            case WRAP -> id -> sortedValues[Mth.positiveModulo(id, length)];
-            case CLAMP -> id -> sortedValues[Mth.clamp(id, 0, length - 1)];
-        };
-    }
-
-    enum OutOfBoundsStrategy {
-        ZERO,
-        WRAP,
-        CLAMP;
-    }
+	enum OutOfBoundsStrategy {
+		ZERO,
+		WRAP,
+		CLAMP;
+	}
 }
