5bfd315f7ae899d4ece122f0ef442091d4cb13fc
lowercasebtw <jejojora2@gmail.com>
2026-01-22 14:23:50 -0500

    s

--- src/main/java/me/alphamode/beta/proxy/rewriter/PacketRewriterEncoder.java
+++ src/main/java/me/alphamode/beta/proxy/rewriter/PacketRewriterEncoder.java
@@ -2,11 +2,14 @@ package me.alphamode.beta.proxy.rewriter;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToMessageEncoder;
+import me.alphamode.beta.proxy.networking.Connection;
+import me.alphamode.beta.proxy.networking.ProxyChannel;
+import me.alphamode.beta.proxy.networking.packet.beta.packets.BetaPacketRegistry;
 import me.alphamode.beta.proxy.networking.packet.beta.packets.BetaRecordPacket;
+import me.alphamode.beta.proxy.networking.packet.modern.packets.ModernRecordPacket;
 
 import java.util.List;
 
-// Proxy -> Client
 public final class PacketRewriterEncoder extends MessageToMessageEncoder<BetaRecordPacket> {
 	private final EncoderRewriter rewriter = new EncoderRewriter();
 
@@ -14,10 +17,31 @@ public final class PacketRewriterEncoder extends MessageToMessageEncoder<BetaRec
 		rewriter.registerPackets();
 	}
 
+	// S -> P
 	@Override
-	protected void encode(final ChannelHandlerContext ctx, final BetaRecordPacket packet, final List<Object> out) throws Exception {
-		IO.println("sending beta packet to modern client");
-		IO.println(packet);
-		out.add(packet);
+	protected void encode(final ChannelHandlerContext context, final BetaRecordPacket packet, final List<Object> out) throws Exception {
+		final BetaPacketRegistry packetRegistry = context.channel().attr(ProxyChannel.BETA_PACKET_REGISTRY_KEY).get();
+		if (packetRegistry == null) {
+			throw new RuntimeException("Cannot encode beta packet as packet-registry is null!");
+		} else {
+			IO.println("sending beta packet to modern client");
+
+			final Connection connection = context.channel().attr(ProxyChannel.CONNECTION_KEY).get();
+			if (connection == null || !connection.isConnected()) {
+				return;
+			}
+
+			IO.println(packet);
+			for (final Class<?> clazz : this.rewriter.clientboundRewriters.keySet()) {
+				if (clazz.isAssignableFrom(packet.getClass())) {
+					final ModernRecordPacket<?> modernPacket = this.rewriter.clientboundRewriters.get(clazz).apply(connection, packet);
+					if (modernPacket != null) {
+						out.add(modernPacket);
+					}
+
+					return;
+				}
+			}
+		}
 	}
 }
