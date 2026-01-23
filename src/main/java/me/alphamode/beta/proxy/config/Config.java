a37a0c3f76291ef9930c7ab42eb3ad55873dc464
lowercasebtw <jejojora2@gmail.com>
2026-01-22 20:30:20 -0500

    Config CODEC

--- src/main/java/me/alphamode/beta/proxy/config/Config.java
+++ src/main/java/me/alphamode/beta/proxy/config/Config.java
@@ -2,6 +2,7 @@ package me.alphamode.beta.proxy.config;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import me.alphamode.beta.proxy.util.codec.StreamCodec;
 import net.lenni0451.mcstructs.text.TextComponent;
 import net.lenni0451.mcstructs.text.serializer.TextComponentSerializer;
 
@@ -12,10 +13,34 @@ import java.nio.file.Paths;
 
 public final class Config {
 	private static final Gson GSON = TextComponentSerializer.LATEST.getGson().newBuilder().setPrettyPrinting().create();
-	public static final String CONFIG_PATH = "config.json";
+	public static final Path path = Paths.get("config.json");
+
+	public static final StreamCodec<JsonObject, Config> CODEC = new StreamCodec<>() {
+		@Override
+		public void encode(final JsonObject object, final Config value) {
+			object.addProperty("brand", value.brand);
+			object.addProperty("message", value.message.asLegacyFormatString());
+			object.addProperty("max_players", value.maxPlayers);
+			object.addProperty("bind_address", value.bindAddress);
+			object.addProperty("bind_port", value.bindPort);
+			object.addProperty("server_address", value.serverAddress);
+		}
+
+		@Override
+		public Config decode(final JsonObject object) {
+			final Config config = new Config();
+			config.brand = object.has("brand") ? object.get("brand").getAsString() : "BetaToModernProxy";
+			config.message = TextComponent.of(object.has("message") ? object.get("message").getAsString() : "A modern to b1.7.3 proxy server");
+			config.maxPlayers = object.has("max_players") ? object.get("max_players").getAsInt() : 20;
+			config.bindAddress = object.has("bind_address") ? object.get("bind_address").getAsString() : "BetaToModernProxy";
+			config.bindPort = object.has("bind_port") ? object.get("bind_port").getAsInt() : 25566;
+			config.serverAddress = object.has("server_address") ? object.get("server_address").getAsString() : "BetaToModernProxy";
+			return config;
+		}
+	};
 
 	private String brand = "BetaToModernProxy";
-	private TextComponent message = TextComponent.of("A modern to b1.7.3 proxy server.", "Test");
+	private TextComponent message = TextComponent.of("A modern to b1.7.3 proxy server.");
 	private int maxPlayers = 20;
 	private String bindAddress = "0.0.0.0";
 	private int bindPort = 25566;
@@ -58,44 +83,29 @@ public final class Config {
 	}
 
 	public void load() {
-		final Path path = Paths.get(CONFIG_PATH);
 		if (!Files.exists(path)) {
-			save();
+			this.save();
+			return;
 		}
 
 		try (final Reader reader = new FileReader(path.toFile())) {
-			final JsonObject config = GSON.fromJson(reader, JsonObject.class);
-			if (config.has("brand")) {
-				this.brand = config.get("brand").getAsString();
-			}
-
-			if (config.has("message")) {
-				this.message = GSON.fromJson(config.get("message"), TextComponent.class);
-			}
-
-			if (config.has("max_players")) {
-				this.maxPlayers = config.get("max_players").getAsInt();
-			}
-
-			if (config.has("bind_address")) {
-				this.bindAddress = config.get("bind_address").getAsString();
-			}
-
-			if (config.has("bind_port")) {
-				this.bindPort = config.get("bind_port").getAsInt();
-			}
-
-			if (config.has("server_address")) {
-				this.serverAddress = config.get("server_address").getAsString();
-			}
+			final Config config = CODEC.decode(GSON.fromJson(reader, JsonObject.class));
+			this.brand = config.brand;
+			this.message = config.message;
+			this.maxPlayers = config.maxPlayers;
+			this.bindAddress = config.bindAddress;
+			this.bindPort = config.bindPort;
+			this.serverAddress = config.serverAddress;
 		} catch (final IOException exception) {
 			throw new RuntimeException(exception);
 		}
 	}
 
 	private void save() {
-		try (final Writer writer = new FileWriter(Paths.get(CONFIG_PATH).toFile())) {
-			GSON.toJson(this, writer);
+		try (final Writer writer = new FileWriter(path.toFile())) {
+			final JsonObject object = new JsonObject();
+			CODEC.encode(object, this);
+			GSON.toJson(object, writer);
 		} catch (final IOException exception) {
 			throw new RuntimeException(exception);
 		}
