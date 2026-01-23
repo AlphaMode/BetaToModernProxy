ad3f338dec9f323067ed1b98752943b146393c58
lowercasebtw <jejojora2@gmail.com>
2026-01-23 16:57:29 -0500

    meow

--- src/main/java/me/alphamode/beta/proxy/networking/RelayChannel.java
+++ src/main/java/me/alphamode/beta/proxy/networking/RelayChannel.java
@@ -6,9 +6,6 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.SimpleChannelInboundHandler;
 import me.alphamode.beta.proxy.BrodernProxy;
-import me.alphamode.beta.proxy.networking.packet.modern.packets.ModernPacketReader;
-import me.alphamode.beta.proxy.networking.packet.modern.packets.ModernPacketWriter;
-import me.alphamode.beta.proxy.networking.packet.modern.packets.ModernRecordPacket;
 
 public final class RelayChannel extends ChannelInitializer<Channel> {
 	private final Connection connection;
@@ -20,17 +17,10 @@ public final class RelayChannel extends ChannelInitializer<Channel> {
 	// Server -> Proxy -> Client
 	@Override
 	protected void initChannel(final Channel channel) {
-//		channel.pipeline().addLast(ModernPacketReader.KEY, new ModernPacketReader());
-		channel.pipeline().addLast(new SimpleChannelInboundHandler<ModernRecordPacket<?>>() {
-			@Override
-			protected void channelRead0(final ChannelHandlerContext ctx, final ModernRecordPacket<?> buf) {
-				BrodernProxy.LOGGER.info("Sending Packet to Client: {}", buf);
-			}
-		});
-		channel.pipeline().addLast(ModernPacketWriter.KEY, new ModernPacketWriter());
 		channel.pipeline().addLast(new SimpleChannelInboundHandler<ByteBuf>() {
 			@Override
 			protected void channelRead0(final ChannelHandlerContext ctx, final ByteBuf buf) {
+				BrodernProxy.LOGGER.info("Sending Packet to Client: {}", buf);
 				connection.write(buf);
 			}
 		});
