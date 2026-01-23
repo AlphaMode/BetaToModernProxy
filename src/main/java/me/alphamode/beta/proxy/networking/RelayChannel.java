f07951ad19c3a26bcb18c45d079f2b05ee493e4b
lowercasebtw <jejojora2@gmail.com>
2026-01-22 21:29:29 -0500

    fixea

--- src/main/java/me/alphamode/beta/proxy/networking/RelayChannel.java
+++ src/main/java/me/alphamode/beta/proxy/networking/RelayChannel.java
@@ -10,6 +10,7 @@ import me.alphamode.beta.proxy.networking.packet.beta.packets.BetaPacketRegistry
 import me.alphamode.beta.proxy.networking.packet.modern.packets.ModernPacketRegistry;
 import me.alphamode.beta.proxy.networking.packet.modern.packets.ModernPacketWriter;
 import me.alphamode.beta.proxy.rewriter.PacketRewriterEncoder;
+import net.raphimc.netminecraft.netty.codec.PacketSizer;
 
 public final class RelayChannel extends ChannelInitializer<Channel> {
 	private final Channel otherChannel;
@@ -30,8 +31,10 @@ public final class RelayChannel extends ChannelInitializer<Channel> {
 		channel.pipeline().addLast(BetaPacketReader.KEY, new BetaPacketReader());
 		// BetaPacket -> ModernPacket
 		channel.pipeline().addLast(PacketRewriterEncoder.KEY, new PacketRewriterEncoder());
-		//  ModernPacket -> ByteBuf
+		// ModernPacket -> ByteBuf
 		channel.pipeline().addLast(ModernPacketWriter.KEY, new ModernPacketWriter());
+		// Writes pre-fixed length
+		channel.pipeline().addLast(new PacketSizer());
 		// ByteBuf -> Client
 		channel.pipeline().addLast(new SimpleChannelInboundHandler<>() {
 			@Override
