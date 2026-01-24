3d826985bc786ce7a716eebdbf851fcbeb5a73db
lowercasebtw <jejojora2@gmail.com>
2026-01-24 16:46:19 -0500

    meow

--- src/main/java/me/alphamode/beta/proxy/networking/packet/modern/packets/ModernPacketWriter.java
+++ src/main/java/me/alphamode/beta/proxy/networking/packet/modern/packets/ModernPacketWriter.java
@@ -1,19 +1,15 @@
 package me.alphamode.beta.proxy.networking.packet.modern.packets;
 
 import io.netty.buffer.ByteBuf;
-import io.netty.buffer.ByteBufAllocator;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.MessageToMessageEncoder;
+import io.netty.handler.codec.MessageToByteEncoder;
 import me.alphamode.beta.proxy.util.codec.ModernStreamCodecs;
 
-import java.util.List;
-
-public class ModernPacketWriter extends MessageToMessageEncoder<ModernRecordPacket<ModernPackets>> {
+public class ModernPacketWriter extends MessageToByteEncoder<ModernRecordPacket<ModernPackets>> {
 	public static final String KEY = "modern-decoder";
 
 	@Override
-	protected void encode(final ChannelHandlerContext context, final ModernRecordPacket<ModernPackets> packet, final List<Object> out) throws Exception {
-		final ByteBuf buf = ByteBufAllocator.DEFAULT.buffer();
+	protected void encode(final ChannelHandlerContext context, final ModernRecordPacket<ModernPackets> packet, final ByteBuf buf) throws Exception {
 		final ModernPackets type = packet.getType();
 		ModernStreamCodecs.VAR_INT.encode(buf, type.getId());
 		try {
@@ -21,6 +17,5 @@ public class ModernPacketWriter extends MessageToMessageEncoder<ModernRecordPack
 		} catch (Exception exception) {
 			exception.printStackTrace();
 		}
-		out.add(buf);
 	}
 }
