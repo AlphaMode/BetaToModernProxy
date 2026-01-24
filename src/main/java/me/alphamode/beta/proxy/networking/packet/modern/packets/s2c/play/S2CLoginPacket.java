ebf6fe2da6306f3cee411b16c9a5d81a2b768e12
lowercasebtw <jejojora2@gmail.com>
2026-01-24 12:16:52 -0500

    fix S2CLoginPacket

--- src/main/java/me/alphamode/beta/proxy/networking/packet/modern/packets/s2c/play/S2CLoginPacket.java
+++ src/main/java/me/alphamode/beta/proxy/networking/packet/modern/packets/s2c/play/S2CLoginPacket.java
@@ -1,16 +1,21 @@
 package me.alphamode.beta.proxy.networking.packet.modern.packets.s2c.play;
 
+import io.netty.buffer.ByteBuf;
 import me.alphamode.beta.proxy.networking.packet.modern.enums.clientbound.ClientboundPlayPackets;
+import me.alphamode.beta.proxy.util.codec.BasicStreamCodecs;
+import me.alphamode.beta.proxy.util.codec.ModernStreamCodecs;
+import me.alphamode.beta.proxy.util.codec.StreamCodec;
 import me.alphamode.beta.proxy.util.data.modern.CommonPlayerSpawnInfo;
+import me.alphamode.beta.proxy.util.data.modern.registry.Registries;
 import me.alphamode.beta.proxy.util.data.modern.registry.ResourceKey;
 import net.lenni0451.mcstructs.core.Identifier;
 
-import java.util.Set;
+import java.util.Collection;
 
 public record S2CLoginPacket(
 		int playerId,
 		boolean hardcore,
-		Set<ResourceKey<Identifier>> levels,
+		Collection<ResourceKey<Identifier>> levels,
 		int maxPlayers,
 		int chunkRadius,
 		int simulationDistance,
@@ -20,31 +25,31 @@ public record S2CLoginPacket(
 		CommonPlayerSpawnInfo commonPlayerSpawnInfo,
 		boolean enforcesSecureChat
 ) implements S2CPlayPacket {
-//	public static final StreamCodec<ByteBuf, S2CLoginPacket> CODEC = StreamCodec.composite(
-//			BasicStreamCodecs.INT,
-//			S2CLoginPacket::playerId,
-//			BasicStreamCodecs.BOOL,
-//			S2CLoginPacket::hardcore,
-//			ModernStreamCodecs.collection(ResourceKey.streamCodec(Registries.DIMENSION)),
-//			S2CLoginPacket::levels,
-//			BasicStreamCodecs.INT,
-//			S2CLoginPacket::maxPlayers,
-//			BasicStreamCodecs.INT,
-//			S2CLoginPacket::chunkRadius,
-//			BasicStreamCodecs.INT,
-//			S2CLoginPacket::simulationDistance,
-//			BasicStreamCodecs.BOOL,
-//			S2CLoginPacket::reducedDebugInfo,
-//			BasicStreamCodecs.BOOL,
-//			S2CLoginPacket::showDeathScreen,
-//			BasicStreamCodecs.BOOL,
-//			S2CLoginPacket::doLimitedCrafting,
-//			CommonPlayerSpawnInfo.CODEC,
-//			S2CLoginPacket::commonPlayerSpawnInfo,
-//			BasicStreamCodecs.BOOL,
-//			S2CLoginPacket::enforcesSecureChat,
-//			S2CLoginPacket::new
-//	);
+	public static final StreamCodec<ByteBuf, S2CLoginPacket> CODEC = StreamCodec.composite(
+			BasicStreamCodecs.INT,
+			S2CLoginPacket::playerId,
+			BasicStreamCodecs.BOOL,
+			S2CLoginPacket::hardcore,
+			ModernStreamCodecs.collection(ResourceKey.streamCodec(Registries.DIMENSION)),
+			S2CLoginPacket::levels,
+			BasicStreamCodecs.INT,
+			S2CLoginPacket::maxPlayers,
+			BasicStreamCodecs.INT,
+			S2CLoginPacket::chunkRadius,
+			BasicStreamCodecs.INT,
+			S2CLoginPacket::simulationDistance,
+			BasicStreamCodecs.BOOL,
+			S2CLoginPacket::reducedDebugInfo,
+			BasicStreamCodecs.BOOL,
+			S2CLoginPacket::showDeathScreen,
+			BasicStreamCodecs.BOOL,
+			S2CLoginPacket::doLimitedCrafting,
+			CommonPlayerSpawnInfo.CODEC,
+			S2CLoginPacket::commonPlayerSpawnInfo,
+			BasicStreamCodecs.BOOL,
+			S2CLoginPacket::enforcesSecureChat,
+			S2CLoginPacket::new
+	);
 
 	@Override
 	public ClientboundPlayPackets getType() {
