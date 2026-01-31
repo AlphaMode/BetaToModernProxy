a095e1430d0d3f9f097ab79eb2b0ba1a48c6a44b
lowercasebtw <jejojora2@gmail.com>
2026-01-30 19:15:34 -0500

    C2SContainerClickPacket

--- src/main/java/me/alphamode/beta/proxy/networking/packet/modern/packets/c2s/play/C2SContainerClickPacket.java
+++ src/main/java/me/alphamode/beta/proxy/networking/packet/modern/packets/c2s/play/C2SContainerClickPacket.java
@@ -1,16 +1,37 @@
 package me.alphamode.beta.proxy.networking.packet.modern.packets.c2s.play;
 
+import io.netty.buffer.ByteBuf;
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 import me.alphamode.beta.proxy.networking.packet.modern.enums.serverbound.ServerboundPlayPackets;
+import me.alphamode.beta.proxy.util.codec.BasicStreamCodecs;
+import me.alphamode.beta.proxy.util.codec.ModernStreamCodecs;
+import me.alphamode.beta.proxy.util.codec.StreamCodec;
 
+// TODO: HashedStack
 public record C2SContainerClickPacket(int containerId, int stateId, short slot, byte button, ClickType clickType,
-									  Object changedSlots, Object carriedItem) implements C2SPlayPacket {
-	// VAR_INT
-	// VAR_INT
-	// SHORT
-	// BYTE
-	// ClickType
-	// Slots
-	// HashedStack
+									  Int2ObjectMap<Object> changedSlots, Object carriedItem) implements C2SPlayPacket {
+	public static final StreamCodec<ByteBuf, Int2ObjectMap<Object>> SLOTS_STREAM_CODEC = ModernStreamCodecs.map(
+			Int2ObjectOpenHashMap::new, BasicStreamCodecs.SHORT.map(Short::intValue, Integer::shortValue), null, 128
+	);
+
+	public static final StreamCodec<ByteBuf, C2SContainerClickPacket> CODEC = StreamCodec.composite(
+			ModernStreamCodecs.VAR_INT,
+			C2SContainerClickPacket::containerId,
+			ModernStreamCodecs.VAR_INT,
+			C2SContainerClickPacket::stateId,
+			BasicStreamCodecs.SHORT,
+			C2SContainerClickPacket::slot,
+			BasicStreamCodecs.BYTE,
+			C2SContainerClickPacket::button,
+			ModernStreamCodecs.javaEnum(ClickType.class),
+			C2SContainerClickPacket::clickType,
+			SLOTS_STREAM_CODEC,
+			C2SContainerClickPacket::changedSlots,
+			null, /* TODO: HashedStack */
+			C2SContainerClickPacket::carriedItem,
+			C2SContainerClickPacket::new
+	);
 
 	@Override
 	public ServerboundPlayPackets getType() {
