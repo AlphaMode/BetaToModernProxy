c7883135a8bac9d3e023af978e40990aa42ac5d0
lowercasebtw <jejojora2@gmail.com>
2026-02-02 13:12:09 -0500

    AddPaintingPacket

--- src/main/java/me/alphamode/beta/proxy/networking/packet/beta/packets/bidirectional/AddPaintingPacket.java
+++ src/main/java/me/alphamode/beta/proxy/networking/packet/beta/packets/bidirectional/AddPaintingPacket.java
@@ -8,13 +8,11 @@ import me.alphamode.beta.proxy.util.codec.BetaStreamCodecs;
 import me.alphamode.beta.proxy.util.codec.StreamCodec;
 import me.alphamode.beta.proxy.util.data.Vec3i;
 
-public record AddPaintingPacket(int id, String motive, Vec3i position, int direction) implements BetaPacket {
-	public static final int MAX_PAINTING_MOTIVE = 13; // 13 is Painting.Motive.length
-	public static final StreamCodec<ByteBuf, String> MOTIVE_CODEC = BetaStreamCodecs.stringUtf8(MAX_PAINTING_MOTIVE);
+public record AddPaintingPacket(int id, Motive motive, Vec3i position, int direction) implements BetaPacket {
 	public static final StreamCodec<ByteBuf, AddPaintingPacket> CODEC = StreamCodec.composite(
 			BasicStreamCodecs.INT,
 			AddPaintingPacket::id,
-			MOTIVE_CODEC,
+			Motive.STRING_CODEC,
 			AddPaintingPacket::motive,
 			Vec3i.CODEC,
 			AddPaintingPacket::position,
@@ -26,5 +24,56 @@ public record AddPaintingPacket(int id, String motive, Vec3i position, int direc
 	@Override
 	public BetaPackets getType() {
 		return BetaPackets.ADD_PAINTING;
+	}
+
+	public enum Motive {
+		KEBAB("Kebab"),
+		AZTEC("Aztec"),
+		ALBAN("Alban"),
+		AZTEC2("Aztec2"),
+		BOMB("Bomb"),
+		PLANT("Plant"),
+		WASTELAND("Wasteland"),
+		POOL("Pool"),
+		COURBET("Courbet"),
+		SEA("Sea"),
+		SUNSET("Sunset"),
+		CREEBET("Creebet"),
+		WANDERER("Wanderer"),
+		GRAHAM("Graham"),
+		MATCH("Match"),
+		BUST("Bust"),
+		STAGE("Stage"),
+		VOID("Void"),
+		SKULL_AND_ROSES("SkullAndRoses"),
+		FIGHTERS("Fighters"),
+		POINTER("Pointer"),
+		PIGSCENE("Pigscene"),
+		BURNING_SKULL("BurningSkull"),
+		SKELETON("Skeleton"),
+		DONKEY_KONG("DonkeyKong");
+
+		public static final int MAX_PAINTING_MOTIVE_NAME_LENGTH = 13;
+		public static final StreamCodec<ByteBuf, Motive> STRING_CODEC = BetaStreamCodecs.stringUtf8(MAX_PAINTING_MOTIVE_NAME_LENGTH).map(Motive::byName, Motive::getName);
+
+		private final String name;
+
+		Motive(final String name) {
+			this.name = name;
+		}
+
+		public String getName() {
+			return this.name;
+		}
+
+		public static Motive byName(final String name) {
+			for (final Motive motive : values()) {
+				if (name.equals(motive.name)) {
+					return motive;
+				}
+			}
+
+			throw new IndexOutOfBoundsException();
+		}
 	}
 }
