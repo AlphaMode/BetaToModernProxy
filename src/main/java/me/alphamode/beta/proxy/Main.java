2dcf45c70724f8f67a0916e9af2838a7c9a0ec84
lowercasebtw <jejojora2@gmail.com>
2026-02-02 18:59:26 -0500

    v

--- src/main/java/me/alphamode/beta/proxy/Main.java
+++ src/main/java/me/alphamode/beta/proxy/Main.java
@@ -1,9 +1,14 @@
 package me.alphamode.beta.proxy;
 
+import me.alphamode.beta.proxy.api.PluginLoader;
 import me.alphamode.beta.proxy.config.Config;
 
 public final class Main {
-	static void main(String[] args) {
+	static void main(final String[] args) {
+		final PluginLoader loader = new PluginLoader();
+		loader.load();
+		loader.onProxyStart();
 		new BrodernProxy(new Config()).listen();
+		loader.onProxyClose();
 	}
 }
